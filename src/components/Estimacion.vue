de3a5c7c53d2b5d0430f97971726a9d9145ef2d5
alejandron00 <38502923+alejandron00@users.noreply.github.com>
2020-04-25 18:52:28 -0500

    intento

--- src/components/Estimacion.vue
+++ src/components/Estimacion.vue
@@ -4,45 +4,53 @@
         <h3>
             Seleccione la cantidad de esfuerzo que desea estima va a tomar para realizar la historia de usuario.
         </h3>
+        <div>
     <table>
         <!-- Debe hacerse con Bootstrap o similar para que sea reactiva.
         es decir, que cambie con el tamaño. -->
         <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>5</td>
+            <button id="btn" value=1 onclick="cambioValorEstimacion" >1</button>
+            <button id="btn" value=2 onclick>2</button>
+             <button id="btn" value=3 onclick>3</button>
+             <button id="btn" value=5 onclick>5</button>
         </tr>
         <tr>
-            <td>8</td>
-            <td>13</td>
-            <td>21</td>
-            <td>34</td>
+            <button id="btn" value=8>8</button>
+            <button id="btn" value=13>13</button>
+             <button id="btn" value =21>21</button>
+            <button id="btn" value=34>34</button>
         </tr>
         <tr>
-            <td>55</td>
-            <td>89</td>
-            <td>∞</td>
-            <td>...</td>
+             <button id="btn" value=55>55</button>
+             <button id="btn" value=99>99</button>
+              <button id="btn" value=∞>∞</button>
+           <button id="btn" value=...>...</button>
         </tr>
     </table>
-
+        </div>
     <div class="estimation">
-        13
+        <h5 id="h5" value=""> {{valor}}</h5>
     </div>
     <div>
         ¿Desea guardar esta estimación?
+        <br>
+            <button>Cancelar</button>
+             <button>Confirmar</button>
     </div>
-    
-
-
 
     </div>
 </template>
 <script>
 export default {
-    
+methods: {
+cambioValorEstimacion(btn){
+btn.value
+        }
+    }
 }
+
+
+
 </script>
 
 <style scoped>
@@ -56,6 +64,13 @@ export default {
         width: 100px;
     }
     .estimation{
-        font-size: 9em;
+        font-size: 5em;
+    }
+    .btn{
+        width: 40px;
+        height: 40px;
+        margin: 5px;
+
+
     }
 </style>
