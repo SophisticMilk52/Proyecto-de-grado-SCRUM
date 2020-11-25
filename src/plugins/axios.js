036b4c6cec5bb2238ecf9fdca80612a4995a6b45
Alvaro Andres Gomez Rey <alvaro.gomez3@correo.icesi.edu.co>
2020-11-25 08:52:26 -0500

    axios.js edited online with Bitbucket

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let local = "http://localhost:8090/scrum/api/"
-let remot = "http://pi2sis.icesi.edu.co/scrum/api/"
+let remot = "https://pi2sis.icesi.edu.co/scrum/api/"
 
 
 	export default axios.create({
