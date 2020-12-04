a695c9e8bd0d35edc215eb5a35d6601104911216
Alvaro Andres Gomez Rey <alvaro.gomez3@correo.icesi.edu.co>
2020-12-03 20:01:36 -0500

    add cors-anywhere link. end my suffering

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 export default Object.freeze({
   // BASE_URL: "http://localhost:8090/scrum/api/",
-   BASE_URL: "https://pi2sis.icesi.edu.co/scrum/api/",
+   BASE_URL: "https://cors-anywhere.herokuapp.com/https://pi2sis.icesi.edu.co/scrum/api/games/70/",
 });
