a5eef809037585b17263c673e145627b5556ee51
Alvaro Andres Gomez Rey <alvaro.gomez3@correo.icesi.edu.co>
2020-12-07 15:29:08 -0500

    death to aagr

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 export default Object.freeze({
-   BASE_URL: "http://localhost:8090/scrum/api/",
-   // BASE_URL: "https://pi2sis.icesi.edu.co/scrum/api/",
+   // BASE_URL: "http://localhost:8090/scrum/api/",
+   BASE_URL: "https://pi2sis.icesi.edu.co/scrum/api/",
 });
