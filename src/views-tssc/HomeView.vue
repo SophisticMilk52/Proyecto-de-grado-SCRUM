8bd6243189773b77bf51072599c6c451f42c0da7
alvaroagr <alvaro.gomez.rey@gmail.com>
2020-12-09 08:17:46 -0500

    add cors dependency, change constant, change ws url

---
+++ src/views-tssc/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="text-center">
+      <h1>Agile-Tool</h1>
+      <h3></h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
